813c1d60dcf7eccf322b307d3f939f81bb61a930
Sophia Dolan <25srdolan@gmail.com>
2025-02-12 16:28:26 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -11,3 +11,17 @@ function calculateInvoice(subtotal, taxRate, discount) {
 console.log("Task 1 Total Invoice 1:", calculateInvoice(100, 0.08, 5)); 
 console.log("Task 1 Total Invoice 2:",calculateInvoice(500, 0.1, 20)); 
 //logged both the invoiuce 
+
+
+// Task 2 - Employee Hourly Wage Calculation
+
+let calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52); 
+    // this creates a function and is the formula for hourlywage 
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`; 
+};
+// returns hourly wage and rounds to two decimals using .tofixed()
+console.log("Task 2 Wage 1:", calculateHourlyWage(52000, 40));
+
+console.log("Task 2 Wage 2:", calculateHourlyWage(75000, 35)); 
+//log hourly wages 
